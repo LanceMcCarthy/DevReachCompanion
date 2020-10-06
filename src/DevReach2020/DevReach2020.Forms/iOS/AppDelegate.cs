789af94a1efef6ed2f83f5b110c79dbd19f00c0a
Lance McCarthy <lance.mccarthy@progress.com>
2020-10-06 18:39:21 -0400

    Update AppDelegate.cs
    Fixed status bar code problem

--- src/DevReach2020/DevReach2020.Forms/iOS/AppDelegate.cs
+++ src/DevReach2020/DevReach2020.Forms/iOS/AppDelegate.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
-using Foundation;
-using Microsoft.AppCenter.Crashes;
+﻿using Foundation;
 using UIKit;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
 
 namespace DevReach2020.Forms.iOS
 {
@@ -13,37 +8,10 @@ namespace DevReach2020.Forms.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            //GlobalExceptionHelper.RegisterForGlobalExceptionHandling();
-
-            if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar 
-                && statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
-            {
-                statusBar.BackgroundColor = Color.FromHex("#404894").ToUIColor();
-            }
-
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new Portable.App());
 
             return base.FinishedLaunching(app, options);
-        }
-    }
-
-    public static class GlobalExceptionHelper
-    {
-        public static void RegisterForGlobalExceptionHandling()
-        {
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
-            {
-                Crashes.TrackError(new System.Exception("CurrentDomainOnUnhandledException",
-                    args.ExceptionObject as System.Exception));
-            };
-
-            TaskScheduler.UnobservedTaskException += (sender, args) =>
-            {
-                Crashes.TrackError(new ApplicationException(
-                    "TaskSchedulerOnUnobservedTaskException",
-                    args.Exception));
-            };
         }
     }
 }
